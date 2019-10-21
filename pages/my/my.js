b93323da41d87542bfdb8a9865eaf871e9fd0af1
FukCup <1224894560@qq.com>
2019-10-21 20:11:13 +0800

    1

--- pages/my/my.js
+++ pages/my/my.js
@@ -18,15 +18,21 @@ Page({
             ],
             [
                 {
-                    id:1,name:'关于'
+                    icon: "http://www.mingyue.musemore.art/storage/api/me/list/我的订单.png",
+                    id: 1,
+                    isShowArrow: true,
+                    isShowPoint: true,
+                    name: "我的客服",
+                    subName: "客服",
+                    type: "DD",
                 }
             ]
         ],
         MyDataInfo:[
-            {id:1,name:'收藏',num:0,type:'jf'},
-            {id:2,name:'地址',num:0,type:'jf'},
-            {id:3,name:'优惠券',num:0,type:'jf'},
-            {id:4,name:'余额',num:0,type:'jf'},
+            {id:1,name:'收藏',num:10,type:'jf'},
+            {id:2,name:'已加入',num:20,type:'jf'},
+            {id:3,name:'待出发',num:40,type:'jf'},
+            {id:4,name:'已出发',num:60,type:'jf'},
         ]
     },
 
