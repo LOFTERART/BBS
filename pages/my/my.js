59cd3e7426e196bcf09ad53920688e743b0a3280
FukCup <1224894560@qq.com>
2019-10-21 20:17:36 +0800

    1

--- pages/my/my.js
+++ pages/my/my.js
@@ -11,8 +11,8 @@ Page({
                     id: 1,
                     isShowArrow: true,
                     isShowPoint: true,
-                    name: "我的订单",
-                    subName: "订单",
+                    name: "我的活动",
+                    subName: "加入的活动",
                     type: "DD",
                 }
             ],
@@ -25,6 +25,24 @@ Page({
                     name: "我的客服",
                     subName: "客服",
                     type: "DD",
+                },
+                {
+                    icon: "http://www.mingyue.musemore.art/storage/api/me/list/我的订单.png",
+                    id: 1,
+                    isShowArrow: true,
+                    isShowPoint: true,
+                    name: "个人信息",
+                    subName: "信息",
+                    type: "DD",
+                },
+                {
+                    icon: "http://www.mingyue.musemore.art/storage/api/me/list/我的订单.png",
+                    id: 1,
+                    isShowArrow: true,
+                    isShowPoint: true,
+                    name: "关于我们",
+                    subName: "关于",
+                    type: "GY",
                 }
             ]
         ],
@@ -37,44 +55,7 @@ Page({
     },
 
     clickCell:function (e) {
-        if(e.currentTarget.dataset.type==='XX'){
-            wx.navigateTo({
-                url: '/pages/withdraw/withdraw'
-            })
-        }else  if(e.currentTarget.dataset.type==='DD'){
-            wx.navigateTo({
-                url: '/pages/order/index/index?type=0'
-            })
-        }else  if(e.currentTarget.dataset.type==='TZ'){
-            wx.navigateTo({
-                url: '/pages/withdraw/withdraw'
-            })
-        }
-        else  if(e.currentTarget.dataset.type==='JF'){
-            wx.navigateTo({
-                url: '/pages/score/score'
-            })
-        }else  if(e.currentTarget.dataset.type==='SC'){
-            wx.navigateTo({
-                url: '/mySub/pages/favorite/index'
-            })
-        }else  if(e.currentTarget.dataset.type==='YHQ'){
-            wx.navigateTo({
-                url: '/pages/coupon/index/index'
-            })
-        }else  if(e.currentTarget.dataset.type==='DZ'){
-            wx.navigateTo({
-                url: '/pages/address/list/list'
-            })
-        }else  if(e.currentTarget.dataset.type==='GWC'){
-            wx.navigateTo({
-                url: '/pages/store/cart/cart'
-            })
-        }else  if(e.currentTarget.dataset.type==='CZW'){
-            wx.navigateTo({
-                url: '/pages/cart/cart'
-            })
-        }else  if(e.currentTarget.dataset.type==='GY'){
+        if(e.currentTarget.dataset.type==='GY'){
             wx.navigateTo({
                 url: '/mySub/pages/about/about'
             })
