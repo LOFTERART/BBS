070a7ea5d31000d22f61bc63be095a08e52a6936
FukCup <1224894560@qq.com>
2019-10-22 15:01:42 +0800

    1

---
+++ KV.js
@@ -0,0 +1,39 @@
+//首页对应KV
+
+let data ={
+
+        //广告
+        banners:[
+        {id:1,image:'https://dcdn.it120.cc/2019/08/22/9815471b-5fe0-496a-8f5e-5cf5ff372eef.jpg',type:'H5',link:'url'},
+        {id:2,image:'https://dcdn.it120.cc/2019/08/22/e38b0442-735d-4df7-a3ce-7f9de997dc45.jpg',type:'H5',link:'url'},
+        {id:3,image:'https://dcdn.it120.cc/2019/08/22/9815471b-5fe0-496a-8f5e-5cf5ff372eef.jpg',type:'sourceCode',link:''},
+        {id:4,image:'https://dcdn.it120.cc/2019/08/22/e38b0442-735d-4df7-a3ce-7f9de997dc45.jpg',type:'H5',link:'url'},
+        ],
+        //金刚位
+        KING:[
+        {id:1,name:'商业',image:'https://dcdn.it120.cc/2019/09/26/ef633ab3-4bf4-47cc-9e47-b12367613398.png',type:'business'},
+        {id:1,name:'交友',image:'https://dcdn.it120.cc/2019/09/26/24362dc4-e21f-466e-9423-efc03156b730.png',type:'business'},
+        {id:1,name:'旅游',image:'https://dcdn.it120.cc/2019/09/26/ef633ab3-4bf4-47cc-9e47-b12367613398.png',type:'business'},
+        {id:1,name:'帮助',image:'https://dcdn.it120.cc/2019/09/26/24362dc4-e21f-466e-9423-efc03156b730.png',type:'business'},
+        {id:1,name:'运动',image:'https://dcdn.it120.cc/2019/09/26/ef633ab3-4bf4-47cc-9e47-b12367613398.png',type:'business'}
+        ],
+        //首页动态列表
+        dynamicList:[
+        {
+        id:1,
+        name:'趣知游第一届香山旅游活动召集令',
+        subName:'2019年 河南人在北京同乡会',
+        tags:'#免费 #旅游 #香山 #吃喝玩乐 #一日游',
+        price:99, //现价
+        originalPrice:199, //原价
+        image:'https://cdn.it120.cc/apifactory/2019/07/14/44db72fe-9c00-4d96-92e3-23ec707becf3.jpg',
+        memNum:10, //已报名成员
+        totalNum:100, //总成员
+        status:{color:'#ff7900',desc:'进行中'},  //动态当前状态    未开始 进行中 已结束
+        author: "趣知游",
+        dateAdd: "2019-10-22 14:26:01",
+        dateUpdate: "2019-10-22 14:29:02",
+        views: 10 //浏览量
+        },
+        ],
+}
