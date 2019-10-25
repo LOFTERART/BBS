3c6f97c7216a0f9874f9c4532d24bb0d7dc792ac
FukCup <1224894560@qq.com>
2019-10-25 13:57:19 +0800

    1

--- pages/together/together.js
+++ pages/together/together.js
@@ -97,6 +97,17 @@ Page({
 
   },
 
+
+
+    //立即参与
+    clickAction:function(e){
+
+        wx.navigateTo({
+            url: '/homeSub/pages/AddBbs/AddBbs'
+        })
+
+    },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
