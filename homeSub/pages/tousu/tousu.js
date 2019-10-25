97a7128f1b0bffbc8b9cc6da429920e8912afe3e
FukCup <1224894560@qq.com>
2019-10-25 12:20:33 +0800

    1

--- homeSub/pages/tousu/tousu.js
+++ homeSub/pages/tousu/tousu.js
@@ -8,12 +8,12 @@ Page({
   data: {
       changeColor:'rgba(231,231,232,1)',
       list: [
-          {id:1,name:'政治敏感'},
-          {id:2,name:'广告营销'},
-          {id:3,name:'淫秽色情'},
-          {id:4,name:'人身攻击'},
-          {id:5,name:'地域攻击'},
-          {id:6,name:'其它'}
+          {id:1,name:'政治敏感',isCheck:false},
+          {id:2,name:'广告营销',isCheck:false},
+          {id:3,name:'淫秽色情',isCheck:false},
+          {id:4,name:'人身攻击',isCheck:false},
+          {id:5,name:'地域攻击',isCheck:false},
+          {id:6,name:'其它',isCheck:false}
           ],
       result: []
 
@@ -23,85 +23,46 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-      console.log(options.id);
       this.setData({
           id:options.id
       })
 
   },
 
-    onChange(event) {
-        console.log(event,"change");
-        this.setData({
-            result: event.detail,
-            changeColor:'rgba(253,96,28,1)'
-        });
+    //点击每一行
+    clickCell:function(e){
+        console.log(e.currentTarget.dataset,'ffffff');
+
+        let index = e.currentTarget.dataset.index;
+        let isCheck= "list["+index+"].isCheck";
+
+
+        if(this.data.list[index].isCheck){
+            this.setData({
+                [isCheck]: !this.data.list[index].isCheck,
+
+            })
+        }else {
+            this.setData({
+                [isCheck]: !this.data.list[index].isCheck,
+
+            })
+
+        }
+
+
     },
 
-    toggle(event) {
-        console.log(event,"log");
-        const { name } = event.currentTarget.dataset;
-        const checkbox = this.selectComponent(`.checkboxes-${name}`);
-        checkbox&&checkbox.toggle();
-    },
 
-    noop() {},
+
 
 
   //  提交作品
 
     clickSubmit:function(e){
-        console.log(e.detail.formId,this.data.result,this.data.id);
+
         UTIL.toast('提交成功');
         // wx.navigateBack();
     },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
