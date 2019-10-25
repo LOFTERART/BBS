e7ae92b72fcde3bf4f656adfbb2cfff7a89c5a67
FukCup <1224894560@qq.com>
2019-10-25 17:41:32 +0800

    1

--- mySub/pages/follow/follow.js
+++ mySub/pages/follow/follow.js
@@ -16,7 +16,14 @@ Page({
             {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
             {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
             {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+        ],
+        conversationList:[
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
         ]
+
 
     },
     onLoad: function(options) {
