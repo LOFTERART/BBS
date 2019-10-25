aa68b64389a34915ad4e0f6b5297f14267491e53
FukCup <1224894560@qq.com>
2019-10-25 11:36:08 +0800

    1

--- pages/together/together.js
+++ pages/together/together.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+      artList:[],
 
   },
 
@@ -12,9 +13,86 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      console.log(options.name);
       wx.setNavigationBarTitle({
         title: options.name
+      })
+
+
+      let arrayArticleList=[];
+      for (let i =0;i<=1;i++){
+          arrayArticleList.push(
+              {
+                  address: "许昌东区森林半岛",
+                  tag:'# 寻找流浪猫',
+                  avatar: "https://dcdn.it120.cc/2019/08/22/1643cf11-1778-4c36-a707-646bf89b2f83.png",
+                  comment: 0,
+                  content: "在小区发现一只流浪喵,有认识的吗?",
+                  follow: 0,
+                  follow_user_id: 3804,
+                  follow_user_type: "teacher",
+                  id: 27565,
+                  image_url_came: [
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                  ],
+                  image_url_came_thumb: [
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                  ],
+                  is_activating: true,
+                  is_del: true,
+                  is_follow: false,
+                  is_like: true,
+                  is_parent: false,
+                  is_top: false,
+                  like: 1,
+                  name: "赵文",
+                  status: "前端开发",
+                  timer: '1天前',
+                  url: "https://www.meishuquanyunxiao.com/share/daily-view.html?daily_id=27565",
+                  views: 0,
+              },
+              {
+                  address: "阳光花墅",
+                  tag:'# 包打听',
+                  avatar: "https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg",
+                  comment: 0,
+                  content: "咱们附件有九阳豆浆机售后吗?",
+                  follow: 0,
+                  follow_user_id: 3804,
+                  follow_user_type: "teacher",
+                  id: 27565,
+                  image_url_came: [
+                      {url:'https://dcdn.it120.cc/2019/10/22/9043ba2f-d4a2-4934-a4d3-acd6d5e8e0e6.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/9043ba2f-d4a2-4934-a4d3-acd6d5e8e0e6.jpg'},
+                  ],
+                  image_url_came_thumb: [
+                      {url:'https://dcdn.it120.cc/2019/10/22/9043ba2f-d4a2-4934-a4d3-acd6d5e8e0e6.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+                      {url:'https://dcdn.it120.cc/2019/10/22/e7d87490-9d84-4433-bbf3-9d61f34b35e2.jpg'},
+
+                  ],
+                  is_activating: true,
+                  is_del: true,
+                  is_follow: false,
+                  is_like: true,
+                  is_parent: false,
+                  is_top: false,
+                  like: 1,
+                  name: "闫莎",
+                  status: "销售",
+                  timer: '刚刚',
+                  url: "https://www.meishuquanyunxiao.com/share/daily-view.html?daily_id=27565",
+                  views: 0,
+              },
+          )
+      }
+
+      this.setData({
+          articleList:arrayArticleList,
       })
 
   },
