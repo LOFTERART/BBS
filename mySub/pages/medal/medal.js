a64c016d10e026fb579e8d0df38c4efd67c5dff2
FukCup <1224894560@qq.com>
2019-10-25 15:40:39 +0800

    1

--- mySub/pages/medal/medal.js
+++ mySub/pages/medal/medal.js
@@ -1,66 +1,30 @@
-// mySub/pages/medal/medal.js
 Page({
+    data: {
+        dataList: [{
+            name: "注册一周内",
+            image: 'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'
+        }, {
+            name: "累计发动态10个",
+            image: 'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'
+        }, {
+            name: "突破100赞数",
+            image: 'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'
+        }, {
+            name: "突破100收藏",
+            image: 'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'
+        }, {
+            name: "评论达人",
+            image: 'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'
+        }, {
+            name: "小区铁粉",
+            image: 'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'
+        }]
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    },
+    detail: function () {
+        wx.showToast({
+            title: '未完成的功能~',
+            icon: "none"
+        })
+    }
 })
