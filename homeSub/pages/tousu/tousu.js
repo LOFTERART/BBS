cd1fa16dcd5271eeebcf17ed57de2f56a8e9fc87
FukCup <1224894560@qq.com>
2019-10-25 12:40:53 +0800

    1

--- homeSub/pages/tousu/tousu.js
+++ homeSub/pages/tousu/tousu.js
@@ -34,15 +34,27 @@ Page({
 
         let index = e.currentTarget.dataset.index;
         let id = e.currentTarget.dataset.id;
-        this.data.result.push(id)
 
         let isCheck= "list["+index+"].isCheck";
 
-        this.setData({
-            [isCheck]: !this.data.list[index].isCheck,
-        })
 
-        console.log(this.data.result,'llll');
+        if(this.data.list[index].isCheck){
+
+            this.setData({
+                [isCheck]: !this.data.list[index].isCheck,
+
+            })
+        }else {
+            this.setData({
+                [isCheck]: !this.data.list[index].isCheck,
+
+            })
+            this.data.result.push(id);
+
+
+        }
+
+        console.log(this.data.result,'hhh');
 
 
     },
