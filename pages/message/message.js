e21ab23d8cdb20ea5dcd7f742673fd0bde00bc75
FukCup <1224894560@qq.com>
2019-10-26 19:32:55 +0800

    1

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,66 +1,49 @@
-// pages/message/message.js
 Page({
+    data: {
+        currentTab: 0,
+        navbar: [
+            {
+            name: "消息"
+            },
+            {
+            name: "动态"
+            },
+            {
+                name: "通知"
+            }
+        ],
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
 
-  },
+        followUserList:[
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+            {id:1,name:'闫莎',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png'},
+        ],
+        conversationList:[
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+            {id:1,name:'您因为睡过头误过哪些事?',image:'https://dcdn.it120.cc/2019/09/26/1437dbec-d951-45a6-8050-89ae11ccd54e.png',viewNum:'6016阅读 - 257状态'},
+        ]
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
+    },
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    },
+    change(e) {
+        console.log(e);
+        this.setData({
+            currentTab: e.detail.index
+        })
+    },
+    goNavBar() {
+        wx.navigateTo({
+            url: "/pages/navbar-1/navbar-1"
+        })
+    }
 })
