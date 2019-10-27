bc72f4b2c5732c89b7d91008eaefd121c76fed97
FukCup <1224894560@qq.com>
2019-10-27 13:45:26 +0800

    1

--- homeSub/pages/ztDetail/ztDetail.js
+++ homeSub/pages/ztDetail/ztDetail.js
@@ -152,7 +152,22 @@ create(store,{
                 url: "https://www.meishuquanyunxiao.com/share/daily-view.html?daily_id=27565",
                 views: 100,
             },
-        ]
+        ],
+
+
+
+        showModalStatus: false,
+        animationData: "",
+        regionArr: [
+            "京", "津", "沪", "渝", "蒙", "新",
+            "藏", "宁", "桂", "港", "澳", "黑",
+            "吉", "辽", "晋", "冀", "青", "鲁",
+            "豫", "苏", "皖", "浙", "闽", "赣",
+            "湘", "鄂", "粤", "琼", "甘", "陕",
+            "黔", "滇", "川"
+        ],
+        regionTxt: "粤",
+        tabIndex: 26,
 
     },
 
@@ -239,6 +254,40 @@ create(store,{
         this. getZTList(true,this.data.id)
     },
 
+
+
+
+    //点击评论
+
+    btnCmt: function() {
+        // 显示遮罩层
+        // 创建动画实例
+        var animation = wx.createAnimation({
+            duration: 220,
+            timingFunction: "linear",
+            delay: 0
+        })
+        //执行第一组动画：Y轴偏移500px后(盒子高度是500px) ，停
+        animation.translateY(500).step()
+        //导出动画对象赋给数据对象储存
+        this.setData({
+            animationData: animation.export(),
+            showModalStatus: true
+        })
+        setTimeout(function() {
+            animation.translateY(0).step()
+            this.setData({
+                animationData: animation.export()
+            })
+        }.bind(this), 200)
+    },
+
+    //关闭评论
+    hideModal: function() {
+        this.setData({
+            showModalStatus: false
+        })
+    },
 
 
     //  评论列表like 点赞
