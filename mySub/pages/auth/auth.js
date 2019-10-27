562a4c6f9fbfff9cb4e0c3b012b5df7589e16242
FukCup <1224894560@qq.com>
2019-10-27 23:35:19 +0800

    1

--- mySub/pages/auth/auth.js
+++ mySub/pages/auth/auth.js
@@ -1,66 +1,72 @@
-// mySub/pages/auth/auth.js
+// const form = require("/component/utils/formValidation.js")
+
+import form  from '../../../component/utils/formValidation.js'
 Page({
+    data: {
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    },
+    onLoad: function(options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    },
+    formSubmit: function(e) {
+        //表单规则
+        let rules = [{
+            name: "name",
+            rule: ["required", "isChinese", "minLength:2", "maxLength:6"], //可使用区间，此处主要测试功能
+            msg: ["请输入姓名", "姓名必须全部为中文", "姓名必须2个或以上字符", "姓名不能超过6个字符"]
+        }, {
+            name: "sex",
+            rule: ["required"],
+            msg: ["请选择性别"]
+        }, {
+            name: "age",
+            rule: ["required", "isNum", "range:[0,150]"],
+            msg: ["请输入年龄", "请输入正确的年龄", "请输入正确的年龄范围：0-150"]
+        }, {
+            name: "mobile",
+            rule: ["required", "isMobile"],
+            msg: ["请输入手机号", "请输入正确的手机号"]
+        }, {
+            name: "email",
+            rule: ["required", "isEmail"],
+            msg: ["请输入邮箱", "请输入正确的邮箱"]
+        }, {
+            name: "idcard",
+            rule: ["required", "isIdCard"],
+            msg: ["请输入身份证号码", "请输入正确的身份证号码"]
+        }, {
+            name: "pwd",
+            rule: ["required", "isEnAndNo"],
+            msg: ["请输入密码", "密码为8~20位数字和字母组合"]
+        }, {
+            name: "pwd2",
+            rule: ["required", "isSame:pwd"],
+            msg: ["请输入确认密码", "两次输入的密码不一致"]
+        }, {
+            name: "range",
+            rule: ["required", "range:[3,20]"],
+            msg: ["请输入区间数字", "请输入3-20之间的数字"]
+        }, {
+            name: "amount",
+            rule: ["required", "isAmount"],
+            msg: ["请输入金额", "请输入正确的金额，允许保留两位小数"]
+        }];
+        //进行表单检查
+        let formData = e.detail.value;
+        let checkRes = form.validation(formData, rules);
+        if (!checkRes) {
+            wx.showToast({
+                title: "验证通过!",
+                icon: "none"
+            });
+        } else {
+            wx.showToast({
+                title: checkRes,
+                icon: "none"
+            });
+        }
+    },
+    formReset: function(e) {
+        console.log("清空数据")
+    }
 })
