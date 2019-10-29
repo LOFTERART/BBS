d0e1a4057acaf4a6af5370c6c95b916a822dedcd
FukCup <1224894560@qq.com>
2019-10-29 22:53:13 +0800

    1

--- homeSub/pages/homeDetail/homeDetail.js
+++ homeSub/pages/homeDetail/homeDetail.js
@@ -17,6 +17,39 @@ Page({
             "https://www.thorui.cn/img/product/8.jpg"
         ],
         bannerIndex: 0,
+
+        //活动详情
+        activityInfo:{
+            isCollection:false, //是否收藏
+            price:99, //现价
+            priceTag:'首届',
+            originalPrice:199, //原价
+            name:'趣知游第一届香山旅游活动召集令',
+            subName:'2019年 河南人在北京同乡会',
+            peoNun:50, //人数
+            signNum:20, //人数
+            CollectionNum:30, //收藏数
+            welfare:[
+                {id:1,tag:'意外保险',des:'旅游意外险'},
+                {id:2,tag:'新品赠送',des:'酒样品200ML'},
+                {id:2,tag:'新品赠送',des:'酒样品200ML'},
+            ],
+            signTime:'2019-10-17 -2019-10-31',//报名时间
+            activeTime:'2019-11-31全天',//活动时间
+            gatherAdd:{address:'北京海淀区人民大学',lat:'39.975529',lng:'116.322402'}, //集合地
+            destination:{address:'北京香山公园',lat:'39.975529',lng:'116.322402'},//目的地
+            tags:['河南同乡','免费一日游','吃喝全包','客车接送'],
+            htmlCon:'<h1>11</h1>'
+        },
+
+
+        //评价list
+        evaluateList:[
+            {id:1,name:'杨过',ava:'url',from:'来自河南许昌',content:'XXXXXXXX'}
+        ],
+
+
+
         menuShow: false,
         popupShow: false,
         value: 1,
