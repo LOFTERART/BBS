0807d4acfaea1e515b5db11fa82832be740695e2
FukCup <1224894560@qq.com>
2019-10-30 17:52:35 +0800

    1

--- pages/aiDynamic/aiDynamic.js
+++ pages/aiDynamic/aiDynamic.js
@@ -9,7 +9,7 @@ create(store,{
    * 页面的初始数据
    */
   data: {
-
+      title:'为你推荐',
       aiCatList:[
           {id:1,name:'活动精选',image:'https://dcdn.it120.cc/2019/10/26/e6a977e7-cecb-45b2-9c8f-7b7512e7a9ea.png',type:'JX'},
           {id:2,name:'热门活动',image:'https://dcdn.it120.cc/2019/10/26/2b4ab83c-5d78-486e-a25e-ff693f00da4d.png',type:'RM'},
