ae0e415187b9a80b5ef1244f9ac0546d3eae63e1
FukCup <1224894560@qq.com>
2019-12-01 13:52:14 +0800

    1

--- homeSub/pages/addPeoInfo/addPeoInfo.js
+++ homeSub/pages/addPeoInfo/addPeoInfo.js
@@ -1,13 +1,85 @@
 // const form = require("/component/utils/formValidation.js")
 
 import form  from '../../../component/utils/formValidation.js'
+
+
+
+
 Page({
   data: {
+      value2: 1,
+      info: [
+          {name:'游客',subName:'点击补全游客信息'}
+      ]
 
   },
+
+
   onLoad: function(options) {
 
   },
+
+    change2: function (e) {
+        console.log(e);
+        this.setData({
+            value2: e.detail.value
+        })
+
+        if(e.detail.type==='reduce'){
+            this.removeItem()
+        }else {
+            this.addItem()
+        }
+
+    },
+
+    addItem: function () {
+        let info = this.data.info;
+        info.push({name:'游客',subName:'点击补全游客信息'});
+        this.setData({
+            info: info
+        });
+    },
+
+    removeItem: function () {
+        let info = this.data.info;
+        info.pop();
+        this.setData({
+            info: info
+        });
+    },
+
+
+
+
+
+
+
+
+
+
+    setPlace: function (e) {
+        let index = parseInt(e.currentTarget.id.replace("place-", ""));
+        let place = e.detail.value;
+        let info = this.data.info;
+        info.details[index].placeName = place;
+        this.setData({
+            info: info
+        });
+    },
+
+    setNumber: function (e) {
+        let index = parseInt(e.currentTarget.id.replace("number-", ""));
+        let number = e.detail.value;
+        let info = this.data.info;
+        info.details[index].number = number;
+        this.setData({
+            info: info
+        });
+    },
+
+
+
   formSubmit: function(e) {
     //表单规则
     let rules = [{
